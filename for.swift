a09d5279d02a0d8b6a169207b4dfe09d81734a33
bae080311 <bae080311@gmail.com>
2024-04-16 19:59:11 +0900

    v3

--- for.swift
+++ for.swift
@@ -1,11 +1,11 @@
-let word = "마인드웨이" //문자열 선언
+let word = "마인드웨이" //단어 "마인드웨이"를 word 변수에 할당합니다.
 
-for i in 0..<word.count { //단어의 길이만큼 반복
-    var line = "" //문자열 변수 선언하고 ""로 초기화 
-    for j in 0..<word.count {  //
-        let index = (i + j) % word.count
-        let char = word[word.index(word.startIndex, offsetBy: index)]
-        line.append(char)
-    }
-    print(line) //루프가 완료되고 완성 된 line 출력
+for i in 0..<word.count { //단어의 문자 수 만큼 반복하는 루프를 시작합니다.
+    var line = "" // 각 라인의 문자열을 저장할 빈 문자열 변수를 선언합니다.
+    for j in 0..<word.count {  // 단어의 문자 수 만큼 반복하는 루프를 시작합니다.
+        let index = (i + j) % word.count // 현재 인덱스를 계산합니다. (원형으로 인덱스를 순환합니다)
+        let char = word[word.index(word.startIndex, offsetBy: index)] //현재 인덱스의 문자를 가져와 변수에 할당합니다.
+        line.append(char) // 현재 문자를 라인에 추가합니다.
+   }
+    print(line) //완성된 라인을 출력합니다.
 }
