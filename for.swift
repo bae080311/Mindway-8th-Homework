b4c7d7f54639a06b79613cd4f25ae9e777cb583b
bae080311 <bae080311@gmail.com>
2024-04-16 19:52:46 +0900

    v1

--- for.swift
+++ for.swift
@@ -1,11 +1,11 @@
-let word = "마인드웨이"
+let word = "마인드웨이" //문자열 선언
 
-for i in 0..<word.count {
-    var line = ""
-    for j in 0..<word.count {
+for i in 0..<word.count { //단어의 길이만큼 반복
+    var line = "" //문자열 변수 선언하고 ""로 초기화 
+    for j in 0..<word.count {  //
         let index = (i + j) % word.count
         let char = word[word.index(word.startIndex, offsetBy: index)]
         line.append(char)
     }
-    print(line)
+    print(line) //루프가 완료되고 완성 된 line 출력
 }
